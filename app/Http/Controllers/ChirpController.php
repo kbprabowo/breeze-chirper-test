40ac6fc75b65d0fadedadfd7c347c55743d7a1d8
kbprabowo <kbprabowo@outlook.com>
2023-04-13 22:24:46 +0800

    feat: add chirps page. can view created chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,7 +15,11 @@ class ChirpController extends Controller
      */
     public function index(): View
     {
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')
+                ->latest()
+                ->get(),
+        ]);
     }
 
     /**
